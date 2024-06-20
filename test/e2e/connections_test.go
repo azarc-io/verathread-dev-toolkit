68d9137caccb03ac93a09f3ec854ab174a1d246f
Wael <wael.jammal@azarc.io>
2024-06-20 20:01:10 +0100

    VTHP-1940: Initial Work

--- test/e2e/connections_test.go
+++ test/e2e/connections_test.go
@@ -2,9 +2,15 @@ package e2e
 
 import (
 	"context"
+	"fmt"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"sync"
 	"testing"
 )
 
@@ -43,5 +49,72 @@ func TestMongoDb(t *testing.T) {
 		}); err != nil {
 			t.Fatalf("failed to insert record in transaction: %s", err)
 		}
+	})
+}
+
+func TestNats(t *testing.T) {
+	uri := "nats://localhost:4222"
+	nc, err := nats.Connect(uri)
+	if err != nil {
+		t.Fatalf("could not connect to nats: %s", err)
+	}
+
+	t.Run("Can create Jetstream Client", func(t *testing.T) {
+		js, err := jetstream.New(nc)
+		if err != nil {
+			t.Fatalf("could not create JetStream client: %s", err)
+		}
+		sif := js.ListStreams(context.Background())
+		if err := sif.Err(); err != nil {
+			t.Fatalf("could not complete request to get streams: %s", err)
+		}
+	})
+
+	t.Run("Can publish and consume a message", func(t *testing.T) {
+		js, err := jetstream.New(nc)
+		if err != nil {
+			t.Fatalf("could not create JetStream client: %s", err)
+		}
+
+		s, err := js.CreateStream(context.Background(), jetstream.StreamConfig{
+			Name:     "TEST_STREAM",
+			Subjects: []string{"FOO.*"},
+		})
+		if err != nil {
+			t.Fatalf("could not create stream: %s", err)
+		}
+
+		cons, err := s.CreateOrUpdateConsumer(context.Background(), jetstream.ConsumerConfig{
+			Durable:   "TestConsumerConsume",
+			AckPolicy: jetstream.AckExplicitPolicy,
+		})
+		if err != nil {
+			t.Fatalf("could not create consumer: %s", err)
+		}
+		if _, err := js.Publish(context.Background(), "FOO.TEST1", []byte("msg")); err != nil {
+			fmt.Println("pub error: ", err)
+		}
+
+		var message jetstream.Msg
+		var wg sync.WaitGroup
+		wg.Add(1)
+		cc, err := cons.Consume(func(msg jetstream.Msg) {
+			message = msg
+			if err := msg.Ack(); err != nil {
+				t.Fatalf("could not ack message: %s", err)
+			}
+			wg.Done()
+		}, jetstream.ConsumeErrHandler(func(consumeCtx jetstream.ConsumeContext, err error) {
+			fmt.Println(err)
+		}))
+		if err != nil {
+			t.Fatalf("could not start consumer: %s", err)
+		}
+		defer cc.Stop()
+
+		wg.Wait()
+
+		require.NotNil(t, message)
+		assert.Equal(t, string(message.Data()), "msg")
 	})
 }
